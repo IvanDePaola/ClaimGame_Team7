85cf6fe2596e169c857031801caa960b92478be4
IvanDePaola <80039205+IvanDePaola@users.noreply.github.com>
2022-05-04 16:16:11 +0200

    Add files via upload
    test

---
+++ Team7_Claim-1/src/main/java/ch/fhnw/inttech/einheit2/Team7Claim1Application.java
@@ -0,0 +1,13 @@
+package ch.fhnw.inttech.einheit2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team7Claim1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team7Claim1Application.class, args);
+	}
+
+}
